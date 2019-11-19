f95dd310dd196d93d01d814277a714dbde27cc6e
ZumbaIdol <david.a.foote@gmail.com>
2019-11-19 01:31:36 -0600

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,18 +6,17 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.valid?
-      @user.save
+    if !@user.save
+      redirect_to '/users/new'
+    else
       session[:user_id] = @user.id
       redirect_to '/'
-    else
-      redirect_to :signup
     end
   end
 
   private
 
   def user_params
-    params_require(:user).permit(:name, :password, :password_confirmation)
+    params_require(:user).permit(:username, :password, :password_confirmation)
   end
 end
